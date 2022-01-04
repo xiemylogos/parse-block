5f40f704f16299cb212eccdc0280696af376317c
xiemylogos <xiemylogos@gmail.com>
2022-01-04 16:17:47 +0800

    update code

---
+++ config.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ontio/ontology/common/log"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	RpcAddr     string
+	BlockHeight uint32
+}
+
+func NewSvrConfig(configFilePath string) (*Config, error) {
+	fileContent, err := ReadFile(configFilePath)
+	if err != nil {
+		log.Errorf("NewSvrConfig: failed, err: %s", err)
+		return nil, err
+	}
+	servConfig := &Config{}
+	err = json.Unmarshal(fileContent, servConfig)
+	if err != nil {
+		log.Errorf("NewSvrConfig: failed, err: %s", err)
+		return nil, err
+	}
+	return servConfig, nil
+}
+
+func ReadFile(fileName string) ([]byte, error) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("ReadFile: open file %s error %s", fileName, err)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Errorf("ReadFile: File %s close error %s", fileName, err)
+		}
+	}()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("ReadFile: ioutil.ReadAll %s error %s", fileName, err)
+	}
+	return data, nil
+}
--- main.go
+++ main.go
@@ -1,84 +1,96 @@
 package main
 
 import (
+	"flag"
 	ontology_go_sdk "github.com/ontio/ontology-go-sdk"
 	"github.com/ontio/ontology/common/log"
 	vconfig "github.com/ontio/ontology/consensus/vbft/config"
 	scom "github.com/ontio/ontology/core/store/common"
 )
 
-func main() {
-	ontSdk := ontology_go_sdk.NewOntologySdk()
-	ontSdk.NewRpcClient().SetAddress("rpc")
-	height, err := ontSdk.GetCurrentBlockHeight()
-	if err != nil {
-		log.Errorf("get current block height panic")
-		panic(err)
-	}
-	if height == 0 {
-		return
-	}
-	block, err := ontSdk.GetBlockByHeight(height)
-	if err != nil {
-		log.Errorf("GetBlockByHeight panic height:%d", height)
-		panic(err)
-	}
-	usedPubKey := make(map[string]bool)
-	for _, bookkeeper := range block.Header.Bookkeepers {
-		pubkey := vconfig.PubkeyID(bookkeeper)
-		if usedPubKey[pubkey] {
-			log.Errorf("duplicate pubkey:%s,height:%d", pubkey, block.Header.Height)
-		}
-		usedPubKey[pubkey] = true
-	}
-	blkInfo, err := vconfig.VbftBlock(block.Header)
-	if err != nil {
-		log.Errorf("VbftBlock panic height:%d", block.Header.Height)
-		panic(err)
-	}
-	log.Info("usedPubKey:%d,height:%d", len(usedPubKey), block.Header.Height)
+var (
+	ServerConfig string
+)
 
-	var chainConfigHeight uint32
-	prevBlock, err := ontSdk.GetBlockByHeight(height - 1)
+func init() {
+	flag.StringVar(&ServerConfig, "cfg", "./config.json", "Config of sigserver")
+}
+
+func main() {
+	cfg, err := NewSvrConfig(ServerConfig)
 	if err != nil {
-		log.Errorf("GetBlockByHeight prevHeader panic height:%d", height-1)
 		panic(err)
 	}
-	if blkInfo.NewChainConfig != nil {
-		prevBlockInfo, err := vconfig.VbftBlock(prevBlock.Header)
+	ontSdk := ontology_go_sdk.NewOntologySdk()
+	ontSdk.NewRpcClient().SetAddress(cfg.RpcAddr)
+	height := cfg.BlockHeight
+	for i := height; i < height; i-- {
+		if i == 0 {
+			log.Info("current height:%d",i)
+			return
+		}
+		block, err := ontSdk.GetBlockByHeight(height)
 		if err != nil {
+			log.Errorf("GetBlockByHeight panic height:%d", height)
 			panic(err)
 		}
-		if prevBlockInfo.NewChainConfig != nil {
-			chainConfigHeight = prevBlock.Header.Height
-		} else {
-			chainConfigHeight = prevBlockInfo.LastConfigBlockNum
+		usedPubKey := make(map[string]bool)
+		for _, bookkeeper := range block.Header.Bookkeepers {
+			pubkey := vconfig.PubkeyID(bookkeeper)
+			if usedPubKey[pubkey] {
+				log.Errorf("duplicate pubkey:%s,height:%d", pubkey, block.Header.Height)
+			}
+			usedPubKey[pubkey] = true
 		}
-	} else {
-		chainConfigHeight = blkInfo.LastConfigBlockNum
-	}
-	chainConfigBlock, err := ontSdk.GetBlockByHeight(chainConfigHeight)
-	if err != nil && err != scom.ErrNotFound {
-		log.Errorf("NewChainConfig is nil height:%d,err:%s", chainConfigHeight, err)
-		panic(err)
-	}
-	if chainConfigBlock == nil {
-		log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
-		panic(nil)
-	}
-	chanConfigBlkInfo, err := vconfig.VbftBlock(chainConfigBlock.Header)
-	if err != nil {
-		log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
-		panic(err)
-	}
-	if chanConfigBlkInfo.NewChainConfig == nil {
-		log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
-		panic(nil)
-	}
-	c := chanConfigBlkInfo.NewChainConfig.C
-	if uint32(len(usedPubKey)) < c+1 && height != 183 {
-		log.Errorf("verify header error:  height:%d,pubkey len:%d,c:%d",
-			height, len(usedPubKey), c)
-		panic(nil)
+		blkInfo, err := vconfig.VbftBlock(block.Header)
+		if err != nil {
+			log.Errorf("VbftBlock panic height:%d", block.Header.Height)
+			panic(err)
+		}
+		log.Info("usedPubKey:%d,height:%d", len(usedPubKey), block.Header.Height)
+
+		var chainConfigHeight uint32
+		prevBlock, err := ontSdk.GetBlockByHeight(height - 1)
+		if err != nil {
+			log.Errorf("GetBlockByHeight prevHeader panic height:%d", height-1)
+			panic(err)
+		}
+		if blkInfo.NewChainConfig != nil {
+			prevBlockInfo, err := vconfig.VbftBlock(prevBlock.Header)
+			if err != nil {
+				panic(err)
+			}
+			if prevBlockInfo.NewChainConfig != nil {
+				chainConfigHeight = prevBlock.Header.Height
+			} else {
+				chainConfigHeight = prevBlockInfo.LastConfigBlockNum
+			}
+		} else {
+			chainConfigHeight = blkInfo.LastConfigBlockNum
+		}
+		chainConfigBlock, err := ontSdk.GetBlockByHeight(chainConfigHeight)
+		if err != nil && err != scom.ErrNotFound {
+			log.Errorf("NewChainConfig is nil height:%d,err:%s", chainConfigHeight, err)
+			panic(err)
+		}
+		if chainConfigBlock == nil {
+			log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
+			panic(nil)
+		}
+		chanConfigBlkInfo, err := vconfig.VbftBlock(chainConfigBlock.Header)
+		if err != nil {
+			log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
+			panic(err)
+		}
+		if chanConfigBlkInfo.NewChainConfig == nil {
+			log.Errorf("NewChainConfig is nil height:%d", chainConfigHeight)
+			panic(nil)
+		}
+		c := chanConfigBlkInfo.NewChainConfig.C
+		if uint32(len(usedPubKey)) < c+1 && height != 183 {
+			log.Errorf("verify header error:  height:%d,pubkey len:%d,c:%d",
+				height, len(usedPubKey), c)
+			panic(nil)
+		}
 	}
 }
